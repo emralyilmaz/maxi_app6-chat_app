0b7f83081ce8e1ef34c75a58121bebde407ee2d6
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-11-30 15:32:52 +0300

    311. Getting Started with Firebase

---
+++ android/app/src/main/kotlin/com/example/maxi_app6_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.maxi_app6_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:maxi_app6_chat_app/screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
