a0dced17ef65a04ca8dd40808e205b0a9df0d102
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-12-01 15:20:21 +0300

    316. Adding Data

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -27,7 +27,11 @@ class ChatScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection("chats/27gYeuVZsiX7xuF67lY9/messages")
+              .add({"text": "This was added by clicking the button! "});
+        },
       ),
     );
   }
