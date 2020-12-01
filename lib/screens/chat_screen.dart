1718fd5914ac074703a7a01a6f8c3c91dc9586db
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-12-01 15:13:22 +0300

    315. Rendering Stream Data with StreamBuilder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,27 +5,29 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text("This works!"),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("chats/27gYeuVZsiX7xuF67lY9/messages")
+            .snapshots(), // snapshots Stream döner: Bu, veri her değiştiğinde yeni değerler yayacağı anlamına gelir.
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]["text"]),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection("chats/27gYeuVZsiX7xuF67lY9/messages")
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((document) {
-              print(document["text"]);
-            });
-            // print(data.docs[0]["text"]);
-          });
-          // snapshots Stream döner: Bu, veri her değiştiğinde yeni değerler yayacağı anlamına gelir.
-        },
+        onPressed: () {},
       ),
     );
   }
