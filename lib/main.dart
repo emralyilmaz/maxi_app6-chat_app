e79795db72ebaa6d84775eff52c1bfca802c7293
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-12-01 16:01:39 +0300

    317. A Basic Authentication Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:maxi_app6_chat_app/screens/auth_screen.dart';
 import 'package:maxi_app6_chat_app/screens/chat_screen.dart';
 
 void main() {
@@ -16,20 +17,20 @@ class MyApp extends StatelessWidget {
         builder: (context, appSnapshot) {
           return MaterialApp(
             title: 'FlutterChat',
-            theme: ThemeData(
-              primarySwatch: Colors.pink,
-              backgroundColor: Colors.pink,
-              accentColor: Colors.deepPurple,
-              accentColorBrightness: Brightness.dark,
-              buttonTheme: ButtonTheme.of(context).copyWith(
-                buttonColor: Colors.pink,
-                textTheme: ButtonTextTheme.primary,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
-            home: ChatScreen(),
+            // theme: ThemeData(
+            //   primarySwatch: Colors.pink,
+            //   backgroundColor: Colors.pink,
+            //   accentColor: Colors.deepPurple,
+            //   accentColorBrightness: Brightness.dark,
+            //   buttonTheme: ButtonTheme.of(context).copyWith(
+            //     buttonColor: Colors.pink,
+            //     textTheme: ButtonTextTheme.primary,
+            //     shape: RoundedRectangleBorder(
+            //       borderRadius: BorderRadius.circular(20),
+            //     ),
+            //   ),
+            // ),
+            home: AuthScreen(),
           );
         });
   }
